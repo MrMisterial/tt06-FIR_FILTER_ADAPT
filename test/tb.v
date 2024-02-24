8700f2efd61b3f39081a7372a9368f3e249cac1f
MrMisterial <markus.philip1999@gmail.com>
2024-02-24 23:11:40 +0100

    testcase adapted

--- test/tb.v
+++ test/tb.v
@@ -23,7 +23,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_haeuslermarkus_fir_filter user_project (
+  tt_um_haeuslermarkus_fir_filter tt_um_haeuslermarkus_fir_filter_inst (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
