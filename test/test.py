5411f64dab143d181ced24e5a61db4ca5233acc3
MrMisterial <markus.philip1999@gmail.com>
2024-02-29 18:09:59 +0100

    all working

--- test/test.py
+++ test/test.py
@@ -6,63 +6,16 @@ from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles, Timer, FallingEdge, ClockCycles, RisingEdge, Join, First
 from cocotb.binary import BinaryValue
 
-async def reset_dut(reset_n, duration_ns):
-	reset_n.value = 0
-	await Timer(duration_ns, units = "ns")
-	reset_n.value = 1
-	reset_n._log.info("dut reset completed")
-
-#@cocotb.test()
-async def tt_init_state(dut):
-	dut._log.info("Start Testcase: Check initial states and outputs")
-	
-	dut.ena.value = 1
-	dut.s_set_coeffs.value = 0
-	dut.s_axis_fir_tvalid.value = 1
-	dut.s_axis_fir_tdata.value = BinaryValue('00000001')
-	
-	
-	#start clock
-	cocotb.start_soon(Clock(dut.clk, 1, units="ns").start())
-	
-	#reset dut
-	await reset_dut(dut.rst_n, 20)
-	
-	await RisingEdge(dut.clk)
-	
-	dut.s_set_coeffs.value = 0
-	dut.s_axis_fir_tvalid.value = 1
-	dut.s_axis_fir_tdata.value = BinaryValue('00000001')
-	
-	
-	await ClockCycles(dut.clk, 100)
-	
-	dut.s_axis_fir_tdata.value = BinaryValue('00000001')
-	
-	await ClockCycles(dut.clk, 10)
-	
-	dut._log.info(f'output val: {dut.m_axis_fir_tdata.value}')
-
-	for i in range(8):
-		dut._log.info(f'#{i:>03} -> {dut.m_axis_fir_tdata.value.binstr}')
-	
-	print_func(dut, 'test')
-
-def print_func(dut, text):
-	dut._log.info(text)
-	
-async def not_called(dut):
-	dut._log.info('this shouldnt be called')
 
 @cocotb.test()
 async def test_impulse_response(dut):
-  dut._log.info("Start")
+  dut._log.info("start testing inital impulse response")
   
-  # Our example module doesn't use clock and reset, but we show how to use them here anyway.
+  #clock config
   clock = Clock(dut.clk, 10, units="us")
   cocotb.start_soon(clock.start())
 
-  # Reset
+  #reset device
   dut._log.info("Reset")
   dut.ena.value = 1
   dut.ui_in.value = 0
@@ -71,17 +24,11 @@ async def test_impulse_response(dut):
   await ClockCycles(dut.clk, 10)
   dut.rst_n.value = 1
 
-  # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test")
-  #dut.ui_in.value = 1
-  #dut.ui_in.value = 1
+  #generate impulse input
   dut.s_axis_fir_tdata.value = 1
   dut.s_axis_fir_tvalid.value = 1
   dut.s_set_coeffs.value = 0
-  #dut.uio_in.value = 30
-  
   await ClockCycles(dut.clk, 10)
-  
   dut.s_axis_fir_tdata.value = 0
   
   #testing if impulse response is correct - expected values - FIR Taps
@@ -121,13 +68,13 @@ async def test_impulse_response(dut):
 @cocotb.test()
 async def test_fir_tap_configuration(dut):
 
-  dut._log.info("Start")
+  dut._log.info("start testing configuration of FIR taps")
   
-  # Our example module doesn't use clock and reset, but we show how to use them here anyway.
-  clock = Clock(dut.clk, 1, units="us") #10
+  #clock config
+  clock = Clock(dut.clk, 10, units="us") 
   cocotb.start_soon(clock.start())
 
-  # Reset
+  #reset device
   dut._log.info("Reset")
   dut.ena.value = 1
   dut.ui_in.value = 0
@@ -137,9 +84,9 @@ async def test_fir_tap_configuration(dut):
   dut.rst_n.value = 1
 
   #config new FIR Filter taps 
-  dut._log.info('Configuring new FIR Filter taps...')
+  dut._log.info('Configuring new FIR Filter taps... \n the configured taps are 3, 2, 1')
   dut.s_set_coeffs.value = 1
-  await ClockCycles(dut.clk, 6) #5
+  await ClockCycles(dut.clk, 6)
   dut.s_axis_fir_tdata.value = 1
   await ClockCycles(dut.clk, 1)
   dut.s_axis_fir_tdata.value = 2
@@ -150,15 +97,13 @@ async def test_fir_tap_configuration(dut):
   dut.s_axis_fir_tdata.value = 0
   dut._log.info('FIR Filter taps configured')
   
+  #test correctness with a impulse response
   
+  #generate impulse
   await ClockCycles(dut.clk, 10)
-
   dut.s_axis_fir_tdata.value = 1
   dut.s_axis_fir_tvalid.value = 1
-  #dut.uio_in.value = 30
-  
   await ClockCycles(dut.clk, 6)
-  
   dut.s_axis_fir_tdata.value = 0
   
   
@@ -199,7 +144,7 @@ async def test_fir_tap_configuration(dut):
   
   dut._log.info(f'impulse response correct!')
   
-  await ClockCycles(dut.clk, 10) #100
+  await ClockCycles(dut.clk, 10)
   
   
   dut._log.info("Testing Step Response")
